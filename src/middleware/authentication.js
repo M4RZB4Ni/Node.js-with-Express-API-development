b6c2b86859579cd3a8a5a49877472ff70ace4ac5
Hamidreza Marzbani <hamied.reza.m@gmail.com>
2023-10-20 13:10:11 +0330

    BEARER parsed

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -4,47 +4,40 @@ require("dotenv").config();
 const secretKey = process.env.AUTH_SECRET_KEY;
 // const secretKey = "c8c3a8b1d1e04ef2a899e8e7a574f40555b62a371bd8f4e9b0a558d599de4fa5";
 
-const verifyToken = (req, res, next) => {
-  // Extract the entire authorization header
-  const authHeader = req.headers["authorization"];
+const verifyToken = (req, res, next) =>
+{
+    // Extract the token from the request headers
+    const token = req.headers['authorization'];
 
-  if (!authHeader) {
-    return res
-      .status(403)
-      .json({ success: false, message: "No token provided." });
-  }
-
-  // Check if the header starts with "Bearer "
-  if (authHeader.startsWith("Bearer ")) {
-    // Remove "Bearer " to extract the actual token
-    const token = authHeader.substring(7);
+    if (!token)
+    {
+        return res.status(403).json({ success: false, message: 'No token provided.' });
+    }
 
     // Verify the token
-    jwt.verify(token, secretKey, (err, decoded) => {
-      if (err) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Failed to authenticate token." });
-      }
+    jwt.verify(token, secretKey, (err, decoded) =>
+    {
+        if (err)
+        {
+            return res.status(401).json({ success: false, message: 'Failed to authenticate token.' });
+        }
 
-      // Save the decoded user information for further use in the request
-      req.user = decoded;
-      next();
+        // Save the decoded user information for further use in the request
+        req.user = decoded;
+
+        // Call next to pass control to the next middleware or route handler
+        next();
     });
-  } else {
-    return res.status(401).json({
-      success: false,
-      message: 'Invalid token format. It should start with "Bearer "',
-    });
-  }
 };
 
-function generateToken(userId, email) {
-  // Payload contains user information that you want to include in the token
-  const payload = {
-    user_id: userId,
-    email: email,
-  };
+
+function generateToken(userId, email)
+{
+    // Payload contains user information that you want to include in the token
+    const payload = {
+        user_id: userId,
+        email: email
+    };
 
   // Sign the token with your secret key and set an expiration time (e.g., 1 hour)
   const token = jwt.sign(payload, secretKey, { expiresIn: "1h" });

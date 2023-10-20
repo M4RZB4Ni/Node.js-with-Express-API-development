a26eaab18af952d40f972a4923421e6b1c7fc21d
Hamidreza Marzbani <hamied.reza.m@gmail.com>
2023-10-20 12:21:05 +0330

    env configured

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,9 +1,7 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-// const secretKey = process.env.AUTH_SECRET_KEY;
-const secretKey =
-  "c8c3a8b1d1e04ef2a899e8e7a574f40555b62a371bd8f4e9b0a558d599de4fa5";
+const secretKey = process.env.AUTH_SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
   // Extract the entire authorization header

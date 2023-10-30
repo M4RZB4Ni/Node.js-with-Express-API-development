6ad3e785c8372eb0751b1ec16da4c4aca5f053d3
Hamidreza Marzbani <hamied.reza.m@gmail.com>
2023-10-30 20:55:33 +0330

    the postController improved to avoid DRY

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,168 +1,129 @@
-const { createPostService, getUserPostsService, updatePostService, deletePostByIdService } = require('../services/postService');
+const {
+  createPostService,
+  getUserPostsService,
+  updatePostService,
+  deletePostByIdService,
+} = require('../services/postService');
 const { sendEmailNotification } = require('../services/emailService');
 const { AppError } = require('../utils/appError');
 
-const createPost = async (req, res,next) =>
+const handleServiceResponse = (res, serviceResult, successStatus = 200) =>
+{
+  if (serviceResult.errors)
+  {
+    // Handle GraphQL errors
+    console.error('GraphQL errors:', serviceResult.errors);
+    throw new AppError(serviceResult.errors[0].message, 500);
+  }
+
+  if (serviceResult.data)
+  {
+    if (res)
+    {
+      res.status(successStatus).json(serviceResult.data);
+    }
+    return serviceResult.data;
+  }
+
+  throw new AppError('Operation failed.', 500);
+};
+
+const createPost = async (req, res, next) =>
 {
   try
   {
-    // Extracted user_id from token
     const { user_id, email } = req.user;
-    // Extract post data from request body
     const { title, content } = req.body;
 
     if (!title || !content)
     {
-      next(new AppError('Please provide title and content.', 400));
+      throw new AppError('Please provide title and content.', 400);
     }
 
     const postCreationResult = await createPostService(user_id, title, content);
+    const post = handleServiceResponse(res, postCreationResult);
 
-    if (postCreationResult.errors)
-    {
-      // Handle GraphQL errors
-      console.error('GraphQL errors:', postCreationResult.errors);
-      next(new AppError(postCreationResult.errors[0].message, 500));
+    sendEmailNotification(email, post.post_id);
 
-    }
-
-    if (postCreationResult.data)
-    {
-      const post = postCreationResult.data.insert_blog_post.returning[0];
-      const response = {
-        success: true,
-        post_id: post.post_id,
-        title: post.title,
-        content: post.content,
-      };
-      sendEmailNotification(email, post.post_id);
-      return res.status(200).json(response);
-    }
-
-
+    res.status(200).json({
+      success: true,
+      post_id: post.post_id,
+      title: post.title,
+      content: post.content,
+    });
   } catch (error)
   {
-    console.error('Error creating post:', error);
-    next(new AppError('Error creating post.', 500));
+    next(error);
   }
 };
 
-const getUserPosts = async (req, res,next) =>
+const getUserPosts = async (req, res, next) =>
 {
   try
   {
-
-    // Extracted user_id from token
     const { user_id } = req.user;
-
     const getPostsResult = await getUserPostsService(user_id);
+    const rawPosts = handleServiceResponse(res, getPostsResult);
 
-    if (getPostsResult.errors)
-    {
-      // Handle GraphQL errors
-      next(new AppError(getPostsResult.errors[0].message, 500));
+    const posts = rawPosts.map((post) => ({
+      title: post.title,
+      content: post.content,
+      id: post.post_id,
+      lastUpdateDate: post.updated_at,
+    }));
 
-    }
-
-    if (getPostsResult.data)
-    {
-
-      const rawPosts = getPostsResult.data.blog_post;
-      const posts = [];
-      rawPosts.forEach((post) =>
-      {
-        posts.push({ title: post.title, content: post.content, id: post.post_id, lastUpdateDate: post.updated_at });
-      });
-      const response = {
-        success: true,
-        posts: posts,
-      };
-      return res.status(200).json(response);
-    }
-
+    res.status(200).json({
+      success: true,
+      posts,
+    });
   } catch (error)
   {
-    console.error('Error fetching user posts:', error);
-    next(new AppError('Error fetching user posts.', 500));
+    next(error);
   }
 };
 
-const editPost = async (req, res,next) =>
+const editPost = async (req, res, next) =>
 {
   try
   {
-
-    // Extracted user_id from token
     const { user_id } = req.user;
-
-    // Extract post data from request body
     const { title, content } = req.body;
     const post_id = req.params.post_id;
 
-
-
-    // Check if required fields are provided
     if (!post_id)
     {
-      next(new AppError('Please provide post_id', 400));
+      throw new AppError('Please provide post_id', 400);
     }
 
     const result = await updatePostService(post_id, user_id, title, content);
+    handleServiceResponse(res, result);
 
-    if (result.errors)
-    {
-      // Handle GraphQL errors
-      console.error('GraphQL errors:', result.errors);
-      next(new AppError(result.errors[0].message, 500));
-    }
-
-    if (result.data)
-    {
-      return res.status(200).json({ success: true, message: result.data });
-    }
-
+    res.status(200).json({ success: true, message: result.data });
   } catch (error)
   {
-    console.error('Error editing post:', error);
-    next(new AppError('Error editing post.', 500));
+    next(error);
   }
 };
 
-const deletePost = async (req, res,next) =>
+const deletePost = async (req, res, next) =>
 {
   try
   {
-
-
-    // Extracted user_id from token
     const { user_id } = req.user;
-
-    // Extract post_id from the request parameters
     const post_id = req.params.post_id;
 
-    // Check if post_id is provided
     if (!post_id)
     {
-      next(new AppError('Please provide post_id.', 400));
+      throw new AppError('Please provide post_id.', 400);
     }
+
     const result = await deletePostByIdService(post_id, user_id);
+    handleServiceResponse(res, result);
 
-    if (result.errors)
-    {
-      // Handle GraphQL errors
-      console.error('GraphQL errors:', result.errors);
-      next(new AppError(result.errors[0].message, 500));
-    }
-
-    if (result.data)
-    {
-      return res.status(200).json({ success: true, message: result.data });
-    }
-
+    res.status(200).json({ success: true, message: result.data });
   } catch (error)
   {
-    console.error('Error deleting post:', error);
-    next(new AppError('Error deleting post.', 500));
+    next(error);
   }
 };
 
@@ -170,5 +131,5 @@ module.exports = {
   createPost,
   getUserPosts,
   editPost,
-  deletePost
+  deletePost,
 };
